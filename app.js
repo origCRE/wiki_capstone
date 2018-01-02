8b52e5438851760007d762920751cc33ad80db21
origCRE <cre@reagan.com>
2018-01-02 16:19:50 -0500

    added final score stuff, close to end of working process

--- app.js
+++ app.js
@@ -4,8 +4,10 @@
 
 var fbData = { 
 	gameCards:[], // defined and controlled from genGameCard()
-	team1Score: 0,
-    team2Score: 0,
+	score:[],
+	gameRound: 1, // track 1 thru 3 rounds of game 
+	// team1Score: 0,
+ //    team2Score: 0,
 	currentCard: 0,
 	numOfCards: 3 // how many cards for game ... will be set by user?
 };	
@@ -15,29 +17,96 @@ function GameCard(cardTitle, cardDesc){
 	this.cardDesc = cardDesc;
 }
 
+function ScoreStat(round, cardID, bucket) {
+	this.round = round;
+	this.cardID = cardID;
+	this.bucket = bucket;
+}
+
 // ---------------------
 // process game data ...
 //----------------------
+function getScore(fbData) {
+	console.log('blah');
+	console.log(fbData.score);
+	scoreStats = {};
+	var team1 = 0;
+	var team2 = 0;
+	var passed = 0;
+
+
+	for (var i=0; i<fbData.score.length;i++){
+    	console.log(i);
+    	console.log(fbData.score[i]['bucket']);
+    	if (fbData.score[i]['bucket'] === "team1") {
+			team1 = team1 +1;
+		} else if (fbData.score[i]['bucket'] === "team2") {
+			team2 = team2 +1;
+		} else {
+			passed = passed +1;
+		} // if block
+	} // end for
+    console.log("team1 tally is: " + team1);
+
+    if (team1 > team2){
+    	console.log("team1 wins");
+    } else if (team1 === team2) {
+    	console.log("no winner");
+    } else {
+    	console.log("team2 wins");
+    }
+	
+	
+	// highScore"
+    // "lowScore"    	
+    // "notAnswered"
+
+
+} // end getScore
+
+function updateScore(fbData, bucket) {
+	var round = fbData.gameRound;
+	cardID = fbData.currentCard;
+    console.log("this is the cardID");
+    console.log(cardID);
+    console.log(bucket);
+	var point = new ScoreStat(round, cardID, bucket);
+    fbData.score.push(point);
+    console.log(fbData.score);
+
+}
 
 function genGameCard(fbData, cardTitle, cardDesc){
 	var card = new GameCard(cardTitle, cardDesc); 
     fbData.gameCards.push(card);
     console.log("new entry!!!!!");
-    for (var i =0; i < fbData.gameCards.length; i++){
-    	console.log(fbData.gameCards[i]);
-    }
+
 	if (fbData.numOfCards === fbData.gameCards.length){
-    	//build transistion function to move to game play...
-    	console.log("3 cards...");
     	triggerGameMode(fbData);
 	}
 } // end selectGameCard func...
+
+function incrementCurrentCard(fbData) {
+	console.log("current card is...");
+	console.log(fbData.currentCard);
+
+	var gameOver = false;
+	if (fbData.numOfCards !== fbData.currentCard +1){ // else triggers on last card 
+		console.log("incrementCurrentCard... length not equal to num of cards!");
+		fbData.currentCard +=1;
+		return gameOver;
+	} else {
+		console.log("not incrementing card... length IS equal to num of cards!");
+		gameOver = true;
+		return gameOver;
+	}
+}
 
 // ---------------------
 // render page
 //----------------------
 function triggerGameMode(fbData){
-	console.log("triggered");
+	// console.log("triggered");
 	$('.gamePlay').removeClass('hideThis');
 	$('.searchDiv').addClass('hideThis');
     displayGameCard(fbData);
@@ -46,16 +115,49 @@ function triggerGameMode(fbData){
 // ---------------------
 // api call and render page for user selections
 //----------------------
-
 function displayGameCard(fbData) {
-	// display cards from this func...
-	console.log(fbData.currentCard);
-	console.log(fbData.gameCards[fbData.currentCard]);
     // format card info based on this ^
-    $('.gamePlay').find('h2').text('test');
-    //find('h2').text();
+    $('#cdTitle').text(fbData.gameCards[fbData.currentCard].cardTitle);
+    $('#cdDesc').text(fbData.gameCards[fbData.currentCard].cardDesc);
+    $('#team1').removeClass('hideThis');
+	$('#team2').removeClass('hideThis');
+	$('#pass').removeClass('hideThis');
+    
+    //set up timer 
+    var time = 3; // adjust time here 
+	function initTimer(){
+		var id = setTimeout(initTimer, 1000); // 
+		$('#timeTgt').html(time);
+		if(time == 0){
+			clearTimeout(id);
+			// when timer hits zero do something...
+			// console.log("do something here :)");
+		    $('#cdTitle').text("Time is up : ) ");
+		    $('#cdDesc').text("Please select the score and then hit the 'Next' button");
+	        $('#team1').addClass('hideThis');
+	        $('#team2').addClass('hideThis');
+	        $('#pass').addClass('hideThis');
+		    var gameOver = incrementCurrentCard(fbData); 
+		    // console.log(gameOver);
+		    if (gameOver !== true){
+                console.log('in the next button trigger func');
+                console.log(gameOver);
+                // only show next button if game not over yet...
+		    	$('#next').removeClass('hideThis');
+		    } else {
+		    	getScore(fbData);
+		    	$('.gamePlay').addClass('hideThis');
+		    	$('.tally').removeClass('hideThis');
+		    	console.log("hit on format for end........");
 
-}
+		    }   	
+		}
+		time --; // set timer countdown interval
+	}
+	initTimer();
+
+
+} // end displayGameCard()
 
 function prepForNextCardGen(){
 	$("#output").empty();
@@ -74,8 +176,8 @@ function getWikiData(searchTerm) {
 			$('#output').html('');
 			for(var i =0; i < data[1].length; i++){
 				var htmlTemplate = "<div><div class='well'><a class='dataHook' href=" +
-			                        data[3][i]+"><h2>"+data[1][i]+ "</h2>" + "<p>" +data[2][i]+
-			                       "</p></a>" + 
+			                        data[3][i]+" target='_blank'><h2>"+data[1][i]+ "</h2>" + "<p>" +data[2][i]+
+			                        "</p></a>" + 
 			                       '<button id="usrChoice" class="btn userSelection">select</button></div></div>';
 				$("#output").prepend(htmlTemplate);  
           }
@@ -101,6 +203,7 @@ $(document).ready(function(){
 		getWikiData(searchTerm);
 	});
 
+   	// need $(document).on() to grab button rendered post initial load...
    	$(document).on("click", ".userSelection", function(){
 		var thisParent = $( this ).parent();
 		var aTag = $(thisParent).find('.dataHook');
@@ -109,6 +212,30 @@ $(document).ready(function(){
         genGameCard(fbData, cardTitle, cardDesc);
         // console.log(fbData.gameCards.length);
         prepForNextCardGen();
+	});
+
+   	// need $(document).on() to grab button rendered post initial load...
+   	$(document).on("click", "#next", function(){
+		console.log("NEXT button was clicked...");
+		displayGameCard(fbData);
+	});
+
+   	// team1
+   	$(document).on("click", "#team1", function(){
+		console.log("team1 button was clicked...");
+        updateScore(fbData, "team1");
+	});
+
+   	// team2
+   	$(document).on("click", "#team2", function(){
+		console.log("team2 button was clicked...");
+		updateScore(fbData, "team2");
+	});
+
+	// pass
+   	$(document).on("click", "#pass", function(){
+		console.log("pass button was clicked...");
+		updateScore(fbData, "pass");
 	});
 
 });	
